b17f236b69be5e0bea4af9c7690742e3c68c7c96
Speedysnail6 <computertutor12@gmail.com>
2013-05-29 20:27:26 -0700

    Added settings.php
    Something I used to debug and view/display the settings for the plugin.
    This will change into some other things later.

---
+++ views/default/plugins/better_settings/settings.php
@@ -0,0 +1,6 @@
+<?php
+$headerbackground = elgg_get_plugin_setting('headerbackground', 'better_settings');
+$headerbackgroundcolor = elgg_get_plugin_setting('headerbackgroundcolor', 'better_settings');
+echo $headerbackground;
+echo '<br>';
+echo $headerbackgroundcolor;
