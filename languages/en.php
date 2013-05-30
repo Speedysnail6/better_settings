7392ea6776aac8379d39bb0596c1b82803696b03
Speedysnail6 <computertutor12@gmail.com>
2013-05-29 20:24:19 -0700

    Added language strings
    Added language strings for:
    Content admin item
    Content: header admin item
    Footer: Better settings

--- languages/en.php
+++ languages/en.php
@@ -6,7 +6,10 @@
 
 $english = array(
 	'admin:appearance:header' => '<span style="color:purple">Header</span>',
+	'admin:content:header' => '<span style="color:purple">Header</span>',
+	'admin:content' => '<span style="color:purple"><b>Content</b></span>',
 	'better_settings:settings:success' => '<b><span style="color:purple">Sucessfully changed settings!</span></b>',
+	'admin:footer:better_settings' => '<span style="color:purple"><b>Better Settings<b></span>',
 );
 
 add_translation('en', $english);
