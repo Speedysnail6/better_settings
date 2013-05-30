f861f6dab661cbc00c7dd8ee76b5d4be63eac2c1
Speedysnail6 <computertutor12@gmail.com>
2013-05-29 20:26:06 -0700

    Admin footer item
    Added purple "Better Settings" admin footer item linking to the plugin
    homepage.

--- start.php
+++ start.php
@@ -1,6 +1,12 @@
 <?php
 function better_settings_init() {
 	    elgg_register_admin_menu_item('configure', 'header', 'appearance');
+	    elgg_register_admin_menu_item('configure', 'header', 'content');
+            elgg_register_menu_item('admin_footer', array(
+               'name' => 'better_settings',
+               'text' => elgg_echo('admin:footer:better_settings'),
+              'href' => 'http://community.elgg.org/pages/view/1370922/better-settings',
+              ));
             $base = elgg_get_plugins_path() . 'better_settings/actions/header.php';
             elgg_register_action("better_settings/header", $base, 'admin');
             elgg_register_js('jscolor', 'mod/better_settings/vendor/jscolor/jscolor.js');
